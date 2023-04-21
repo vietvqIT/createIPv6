7453321328975c898e9cf400be9bae3ea654838e
vietvqIT <124752132+vietvqIT@users.noreply.github.com>
2023-04-21 19:42:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,16 +51,7 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-upload_proxy() {
-    local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
-
-}
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
@@ -117,4 +108,4 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-upload_proxy
+
